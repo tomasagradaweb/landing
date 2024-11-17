6836b827a3033f28be32613a20ba03f3db09693c
Tomas MacOS <tomas@agradaweb.com>
2024-11-17 16:18:00 +0100

    Fix. Ahora el build si funciona

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  output: 'static', // Asegúrate de que el sitio sea estático
   vite: {
     resolve: {
       alias: {
-        '@assets': '/src/assets', // Asegúrate de que esta ruta sea la correcta
+        '@assets': '/src/assets', // Asegúrate de que esta ruta sea válida
       },
     },
   },
